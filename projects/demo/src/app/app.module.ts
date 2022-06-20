a516723ed340a1d9c65e60b9f06f122f3843e1ae
chiradenuwan <chiranjayadenuwan@gmail.com>
2022-06-20 17:33:13 +0530

    create a test project to view ngx-nic-parser

---
+++ projects/demo/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {NgxNicParserModule} from "ngx-nic-parser";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxNicParserModule
+  ],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
